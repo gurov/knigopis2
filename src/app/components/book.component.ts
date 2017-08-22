dd654f349549405db06dbe2b95f8ecbaec60c309
Pavel Gurov <lucius.gu@ya.ru>
2017-08-22 08:00:19 +0700

    Hide book button for not auth user

--- src/app/components/book.component.ts
+++ src/app/components/book.component.ts
@@ -1,19 +1,24 @@
 import { Book } from '../models';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { AuthService } from "../services/auth.service";
 
 @Component({
     selector: 'k-book',
     templateUrl: './book.component.html'
 })
-export class BookComponent implements OnInit {
+export class BookComponent implements OnChanges {
     @Input() book: Book = new Book();
     public isMyBook: boolean = true;
+    public isAuth: boolean = false;
 
     constructor(private authService: AuthService) {
     }
 
     ngOnInit() {
+        this.authService.isAuthorized.subscribe(isAuth => this.isAuth = isAuth);
+    }
+
+    ngOnChanges() {
         this.isMyBook = this.authService.getCurrentUser() && this.authService.getCurrentUser().id === this.book.userId;
     }
 
